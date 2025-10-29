5929ea56b6dfcd3874a39a2728d2c186b458d207
BasselMahmoudD <bassel.mahmoud88@outlook.com>
2025-10-29 20:19:46 +0300

    feat(src/components/home/herosection): add hero section to home page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import HeroSection from "./pages/Home/components/HeroSection";
 
 function App() {
   return (
-    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-      Button
-    </button>
+    <>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
+    </>
   );
 }
 
