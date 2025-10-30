f762793b92b2854786ea05ebf419624a860dcc9b
OMAR WAEL <omarwael012@gmail.com>
2025-10-30 13:53:59 +0300

    style(all files): apply pretter formater
    clear white spaces adjust tab indent use single quote

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
-import "./App.css";
+import './App.css';
+import Menu from './pages/Home/components/menu';
 
 function App() {
-  return (
-    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-      Button
-    </button>
-  );
+    return (
+        // <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        //   Button
+        // </button>
+        <Menu></Menu>
+    );
 }
 
 export default App;
