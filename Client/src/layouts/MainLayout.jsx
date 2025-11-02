9509010bac5e0b663e148014383764a77b20587d
OMAR WAEL <omarwael012@gmail.com>
2025-11-02 23:51:08 +0200

    feat(app.jsx): add global react-hot-toaster-component

--- Client/src/layouts/MainLayout.jsx
+++ Client/src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Toaster } from 'react-hot-toast';
 export default function MainLayout() {
     return (
         <>
+            <Toaster></Toaster>
             <Navbar />
             <main>
                 <Outlet />
