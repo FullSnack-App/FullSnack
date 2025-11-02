edb4f57942a4f7a46e2a796cad21593b5ccd289a
OMAR WAEL <omarwael012@gmail.com>
2025-11-02 22:18:18 +0200

    feat(pages/offer): add offer section in homepage , offerpage as seprate offer details page

--- Client/src/router/AppRouter.jsx
+++ Client/src/router/AppRouter.jsx
@@ -5,12 +5,13 @@ import Menu from '../pages/Menu';
 import Contact from '../pages/Contact';
 import Checkout from '../pages/checkout';
 import About from '../pages/About';
-
+import Offer from '../pages/Offer'
 export const routes = (
     <Routes>
         <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/menu" element={<Menu />} />
+            <Route path="/offer/:id" element={<Offer/>} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/about" element={<About />} />
