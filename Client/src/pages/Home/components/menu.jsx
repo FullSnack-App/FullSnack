db31633bd3a847e664f772e850a42202af711784
OMAR WAEL <omarwael012@gmail.com>
2025-11-02 22:19:36 +0200

    feat(pages/menu/home): add offers section in the homepage

--- Client/src/pages/Home/components/menu.jsx
+++ Client/src/pages/Home/components/menu.jsx
@@ -119,12 +119,7 @@ function Menu() {
     return (
         <>
             <div
-                className="mx-auto px-4 py-16 w-full
-            sm:max-w-[540px]
-            md:max-w-[720px]
-            lg:max-w-[960px]
-            xl:max-w-[1140px]
-            2xl:max-w-[1320px]"
+                className="container"
             >
                 {/* Section Title */}
                 <div className="title text-center mb-12">
