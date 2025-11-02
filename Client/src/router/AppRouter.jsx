d0f56f58468d720a8a0f27058cb5b92b840c7c5b
BasselMahmoudD <bassel.mahmoud88@outlook.com>
2025-11-02 23:25:58 +0200

    feat(components): create admin dashboard
    create admindashboard to manage menuItem and offers

--- Client/src/router/AppRouter.jsx
+++ Client/src/router/AppRouter.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router';
 import MainLayout from '../layouts/MainLayout';
+import Admin from '../layouts/Admin';
 import Home from '../pages/Home';
 import Menu from '../pages/Menu';
 import Contact from '../pages/Contact';
 import Checkout from '../pages/checkout';
 import About from '../pages/About';
+import AdminMenuItem from '../pages/Admin/components/AdminMenuItem';
+import AdminOffers from '../pages/Admin/components/AdminOffers';
+import AdminDashboard from '../pages/Admin/Dashboard';
 
 export const routes = (
     <Routes>
@@ -14,6 +18,11 @@ export const routes = (
             <Route path="/contact" element={<Contact />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/about" element={<About />} />
+        </Route>
+        <Route path="/admin" element={<Admin />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="menu" element={<AdminMenuItem />} />
+            <Route path="offers" element={<AdminOffers />} />
         </Route>
         <Route path="*" element={<div>Page Not Found (blank)</div>} />
     </Routes>
