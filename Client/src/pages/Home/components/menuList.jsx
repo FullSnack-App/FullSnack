34355b030103fe5f0306220bae711721c27d49f3
OMAR WAEL <omarwael012@gmail.com>
2025-11-04 00:36:49 +0200

    refactor(menu.jsx): make the menucomp use the menucontext instead of static data

--- Client/src/pages/Home/components/menuList.jsx
+++ Client/src/pages/Home/components/menuList.jsx
@@ -1,14 +1,23 @@
-import MenuItem from './MenuItem';
 import SkeletonMenuItem from '../../../components/SkeletonMenuItem';
+import MenuItem from './menuItem';
 
-function MenuList({ menulist, className = '' }) {
+function MenuList({ menulist = [], loading, className = '' }) {
     return (
         <div
             className={`grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xlg:grid-cols-4 transition-all duration-300 ${className}`}
         >
-            {menulist.length > 0
-                ? menulist.map((menuitem) => <MenuItem key={menuitem._id} {...menuitem} />)
-                : [...Array(3)].map((_, i) => <SkeletonMenuItem key={i} />)}
+            {loading ? (
+                // While loading → show skeleton placeholders
+                [...Array(6)].map((_, i) => <SkeletonMenuItem key={i} />)
+            ) : menulist.length > 0 ? (
+                // When loaded and have data
+                menulist.map((menuitem) => <MenuItem key={menuitem._id} {...menuitem} />)
+            ) : (
+                // When loaded but empty
+                <p className="col-span-full text-center text-gray-500 text-lg">
+                    No menu items found.
+                </p>
+            )}
         </div>
     );
 }
