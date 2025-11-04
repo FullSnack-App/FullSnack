a0a696201a5d55bca4415a63b73537e31bc03569
OMAR WAEL <omarwael012@gmail.com>
2025-11-04 09:57:42 +0200

    fix: correct import case sensitivity for MenuProvider in main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { MenuProvider } from "./context/MenuProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import { MenuProvider } from "./context/menuProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
     <MenuProvider>
