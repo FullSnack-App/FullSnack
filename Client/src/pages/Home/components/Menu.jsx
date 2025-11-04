da702de90cffb6980d7bcfe20e08aa0d69c551b3
Peter Torki <pjturki@gmail.com>
2025-11-05 00:24:33 +0200

    refactor: update import paths for consistency and remove unused TypeScript entry from package-lock

--- Client/src/pages/Home/components/Menu.jsx
+++ Client/src/pages/Home/components/Menu.jsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react';
-import TogglerGroup from '../../../components/ToggleGroup';
+import TogglerGroup from '../../../components/TogglerGroup';
 import MenuList from './MenuList';
 import { useMenu } from '../../../hooks/useMenu';
 
